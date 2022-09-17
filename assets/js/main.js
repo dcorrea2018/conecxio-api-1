ed37d3cbc229dcd186fe63cd3484fc773afddfbc
dcorrea2018 <38849053+dcorrea2018@users.noreply.github.com>
2022-09-17 08:39:06 -0700

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 console.log("Entro al main.js");
+
+const imgRandom = document.getElementById('imgRandom')
+
+const randomImage = () => {
+
+
+    console.log("entro a la funcion randomImage");
+    const URL = "https://picsum.photos/200/300";
+
+
+    fetch(URL)
+        .then((data) => {
+            console.log(data);
+            imgRandom.src = data.url;
+        })
+        .catch((error) => {
+            console.log("Ocurrio un error!");
+        });
+}
